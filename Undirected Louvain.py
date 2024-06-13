b5c2679f1d9ce6fd5a6a4ef3733b818532831df8
LKantoro <107733382+LKantoro@users.noreply.github.com>
2024-06-13 11:54:01 -0700

    Update Undirected Louvain.py

--- Undirected Louvain.py
+++ Undirected Louvain.py
@@ -19,44 +19,6 @@ def louvain(
     weight: str = "weight",
     resolution: float = 1.0,
     randomize: int = None) -> NodeClustering:
-    """
-    Louvain  maximizes a modularity score for each community.
-    The algorithm optimises the modularity in two elementary phases:
-    (1) local moving of nodes;
-    (2) aggregation of the network.
-    In the local moving phase, individual nodes are moved to the community that yields the largest increase in the quality function.
-    In the aggregation phase, an aggregate network is created based on the partition obtained in the local moving phase.
-    Each community in this partition becomes a node in the aggregate network. The two phases are repeated until the quality function cannot be increased further.
-
-
-    **Supported Graph Types**
-
-    ========== ======== ========
-    Undirected Directed Weighted
-    ========== ======== ========
-    Yes        No       No
-    ========== ======== ========
-
-    :param g_original: a networkx/igraph object
-    :param weight: str, optional the key in graph to use as weight. Default to 'weight'
-    :param resolution: double, optional  Will change the size of the communities, default to 1.
-    :param randomize: int, RandomState instance or None, optional (default=None). If int, random_state is the seed used by the random number generator; If RandomState instance, random_state is the random number generator; If None, the random number generator is the RandomState instance used by `np.random`.
-    :return: NodeClustering object
-
-
-    :Example:
-
-    >>> from cdlib import algorithms
-    >>> import networkx as nx
-    >>> G = nx.from_numpy_array(MatrixA)
-    >>> coms = algorithms.louvain(G, weight='weight', resolution=1.)
-
-    :References:
-
-    Blondel, Vincent D., et al. `Fast unfolding of communities in large networks. <https://iopscience.iop.org/article/10.1088/1742-5468/2008/10/P10008/meta/>`_ Journal of statistical mechanics: theory and experiment 2008.10 (2008): P10008.
-
-    .. note:: Reference implementation: https://github.com/taynaud/python-louvain
-    """
     g = g_original
     coms = community_louvain.best_partition(
         g, weight=weight, resolution=resolution, randomize=randomize
